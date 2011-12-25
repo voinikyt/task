4722ef61327575aac0e534ca353d0705bd77d488
voinikyt <voinikyt@gmail.com>
2011-12-26 00:38:09 +0200

    Using a new version of srinkWrap API and hanging the entity class name

--- task-ejb/src/test/java/com/ivo/ejb/CreateUserEJBTest.java
+++ task-ejb/src/test/java/com/ivo/ejb/CreateUserEJBTest.java
@@ -3,25 +3,27 @@ package com.ivo.ejb;
 import static org.junit.Assert.*;
 
 import javax.ejb.EJB;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 
 import org.jboss.arquillian.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ArchivePaths;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import com.ivo.ejb.entities.Employee;
+
 @RunWith(Arquillian.class)
 public class CreateUserEJBTest {
 
-	@Deployment
-	public static JavaArchive createTestArchive() {
-		return ShrinkWrap.create(JavaArchive.class, "test.jar")
-				.addClass(CreateUserEJB.class)
-				.addAsResource("test-persistence.xml", "META-INF/persistence.xml");
-	}
+    @Deployment
+    public static Archive<?> createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class, "foo.jar")
+                .addClass(CreateUserEJB.class)
+                .addManifestResource("test-persistence.xml", ArchivePaths.create("persistence.xml"));
+    }
 
 	@EJB
 	CreateUserEJB createUserEJB;
@@ -29,8 +31,10 @@ public class CreateUserEJBTest {
 	@Test
 	public void testCreate() {
 		assertNotNull(createUserEJB);
-		//assertNotNull(em);
-//		createUserEJB.create("name");
+		Employee emp = new Employee();
+		emp.setName("Ivo");
+		createUserEJB.create(emp);
+		assertNotNull(new Long(emp.getId()));
 	}
 
 }
