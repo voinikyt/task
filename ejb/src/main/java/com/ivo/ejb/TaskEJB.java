401029060da9e6d5c6ba3c81b0b2785892c52624
voinikyt <voinikyt@gmail.com>
2012-01-24 20:20:01 +0200

    DAO EJB classes

---
+++ ejb/src/main/java/com/ivo/ejb/TaskEJB.java
@@ -0,0 +1,62 @@
+package com.ivo.ejb;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+import javax.annotation.security.DeclareRoles;
+import javax.annotation.security.RolesAllowed;
+import javax.ejb.SessionContext;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import com.ivo.ejb.entities.Task;
+
+@Stateless
+@DeclareRoles({"admin"})
+public class TaskEJB {
+
+	@PersistenceContext(name = "task")
+	private EntityManager em;
+
+	@Resource
+	SessionContext sessionContext;
+
+	@RolesAllowed({"admin"})
+	public Task createTask(Task task) {
+		if (task == null) throw new IllegalArgumentException("You cannot create a null task");
+		em.persist(task);
+
+		System.out.println("ROLE: " + sessionContext.getCallerPrincipal().getName());
+
+		return task;
+	}
+
+	public Task updateTask(Task task) {
+		if (task == null) throw new IllegalArgumentException("You cannot create a null task");
+		Task managedTask = em.merge(task);
+		return managedTask;
+	}
+
+	public void removeTask(Long taskId) {
+		if (taskId == null) throw new IllegalArgumentException("taskId cannot e null");
+		Task task = em.find(Task.class, taskId);
+		em.remove(task);
+		em.flush();
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<Task> getAll() {
+		Query query = em.createQuery("SELECT t from Task as t");
+		return query.getResultList();
+	}
+
+	public Task findByID(Long taskId) {
+		if (taskId == null) throw new IllegalArgumentException("taskId cannot e null");
+		Query query = em.createQuery("SELECT t from Task as t where t.id = :taskId");
+		query.setParameter("taskId", taskId);
+		return (Task) query.getSingleResult();
+	}
+
+}
---
+++ ejb/src/main/java/com/ivo/ejb/TaskStatusEJB.java
@@ -0,0 +1,54 @@
+package com.ivo.ejb;
+
+import java.util.List;
+
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import com.ivo.ejb.entities.TaskStatus;
+
+/**
+ * State-less EJB controlling persistence and logic operations with {@link TaskStatus} objects
+ * @author ikolev
+ */
+@Stateless
+public class TaskStatusEJB {
+	@PersistenceContext(name = "task")
+	public EntityManager em;
+
+	/**
+	 * Persists a new TaskStatus object
+	 * @param taskStatus
+	 * @return
+	 * @throws IllegalArgumentException if a the parameter is null
+	 */
+	public TaskStatus addTaskStatus(TaskStatus taskStatus) {
+		if (taskStatus == null) throw new IllegalArgumentException("taskStatus cannot be null");
+		em.persist(taskStatus);
+		return taskStatus;
+	}
+
+	/**
+	 * Removes a TaskStatus object from the database
+	 * @param taskStatus
+	 * @throws IllegalArgumentException if the parameter is null or the parameter's id is null
+	 */
+	public void removeTaskStatus(TaskStatus taskStatus) {
+		if (taskStatus == null || taskStatus.getId() == null) throw new IllegalArgumentException("taskStatus cannot be null");
+		TaskStatus managedTaskStatus = em.find(TaskStatus.class, taskStatus.getId());
+		em.remove(managedTaskStatus);
+	}
+
+	/**
+	 * Returns all TaskStatus objects from the persistence context
+	 * @return a {@link List} of TaskStatus objects ordered by name
+	 */
+	@SuppressWarnings("unchecked")
+	public List<TaskStatus> getAllStatuses() {
+		Query query = em.createQuery("SELECT ts FROM TaskStatus as ts ORDER BY ts.name ");
+		return query.getResultList();
+	}
+}
