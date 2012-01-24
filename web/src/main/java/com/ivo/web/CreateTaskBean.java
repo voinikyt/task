5d082d4f3a54c4294734c723beb27a301b69181a
voinikyt <voinikyt@gmail.com>
2012-01-24 20:28:29 +0200

    init

---
+++ web/src/main/java/com/ivo/web/CreateTaskBean.java
@@ -0,0 +1,12 @@
+package com.ivo.web;
+
+import javax.faces.bean.ManagedBean;
+
+
+@ManagedBean(name = "task")
+public class CreateTaskBean {
+
+	public String getMessage() {
+		return "hello";
+	}
+}
