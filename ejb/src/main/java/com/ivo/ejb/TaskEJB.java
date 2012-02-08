d17d1245bc4c28f478de15b4653092927dea5f34
Ivaylo <voinikyt@gmail.com>
2012-02-08 17:17:20 +0200

    Delete programmatic validation

--- ejb/src/main/java/com/ivo/ejb/TaskEJB.java
+++ ejb/src/main/java/com/ivo/ejb/TaskEJB.java
@@ -2,8 +2,6 @@ package com.ivo.ejb;
 
 import java.util.List;
 
-import javax.annotation.Resource;
-import javax.ejb.SessionContext;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -17,26 +15,17 @@ public class TaskEJB {
 	@PersistenceContext(name = "task")
 	private EntityManager em;
 
-	@Resource
-	SessionContext sessionContext;
-
 	public Task createTask(Task task) {
-		if (task == null) throw new IllegalArgumentException("You cannot create a null task");
 		em.persist(task);
-
-		System.out.println("ROLE: " + sessionContext.getCallerPrincipal().getName());
-
 		return task;
 	}
 
 	public Task updateTask(Task task) {
-		if (task == null) throw new IllegalArgumentException("You cannot create a null task");
 		Task managedTask = em.merge(task);
 		return managedTask;
 	}
 
 	public void removeTask(Long taskId) {
-		if (taskId == null) throw new IllegalArgumentException("taskId cannot e null");
 		Task task = em.find(Task.class, taskId);
 		em.remove(task);
 		em.flush();
@@ -49,10 +38,8 @@ public class TaskEJB {
 	}
 
 	public Task findByID(Long taskId) {
-		if (taskId == null) throw new IllegalArgumentException("taskId cannot e null");
 		Query query = em.createQuery("SELECT t from Task as t where t.id = :taskId");
 		query.setParameter("taskId", taskId);
 		return (Task) query.getSingleResult();
 	}
-
 }
