d52fba1505c29da736134534bb190c39de8c8dd0
Ivaylo Kolev <voinikyt@gmail.com>
2012-07-06 08:58:14 +0300

    final

---
+++ web/src/main/webapp/resources/javascript/task.js
@@ -0,0 +1,10 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+function handleCreateUserSubmit(xhr, status, args) {
+    if (!args.validationFailed) {
+        $('#userFromPanel').hide();
+    }
+}
+
